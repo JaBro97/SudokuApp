b91afdfbb0ac2022acd016a6fe25d39c4b9909f6
JaBro97 <66781236+JaBro97@users.noreply.github.com>
2020-06-16 18:28:43 +0200

    gameStart

--- app/src/main/java/com/example/sudoku/MainActivity.java
+++ app/src/main/java/com/example/sudoku/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.sudoku;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 
 public class MainActivity extends AppCompatActivity {
+
+    CheckBox checkBoxSimple;
+
+    Button btnStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkBoxSimple = findViewById(R.id.checkBoxSimple);
+        btnStart = findViewById(R.id.btnStart);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(checkBoxSimple.isChecked()){
+                    Intent startGameEasy = new Intent(MainActivity.this, Game.class);
+                    startGameEasy.putExtra("Easy", 1);                                  //1 ist einfach | 2 ist mittel | 3 ist schwer
+                    startActivity(startGameEasy);
+                }
+                //HIER MUSS DANN MITEL UND SCHWER EINGEFÜGT WERDEN
+                else{
+                    //do nothing
+                }
+            }
+        });
     }
 }
